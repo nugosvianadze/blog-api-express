dae65617d7e969aceaed712e821fd0bdd1b83c12
Misha-Kirichenko <m.kirichenko.dev@gmail.com>
2023-10-16 10:36:57 +0400

    user logic added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const categoryRouter = require("./CategoryRouter");
+const userRouter = require("./UserRouter");
 
 router.use("/category", categoryRouter);
-
+router.use("/user", userRouter);
 module.exports = router;
