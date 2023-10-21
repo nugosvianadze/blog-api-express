ff74a3d7bd9f3e98baf014d38116f768770eabc1
TCruzy <nugzar.svianadze.1@btu.edu.ge>
2023-10-21 23:21:49 +0400

    changed validatePost.js

--- src/middleware/validatePost.js
+++ src/middleware/validatePost.js
@@ -2,7 +2,7 @@ const validatePost = (req, res, next) => {
     const { title, description, category } = req.body;
     console.log(title, description, category)
     if (!title || !description || !category) {
-        return res.send(400).send({ msg: 'Please provide title, description and category' });
+        return res.send({ msg: 'All fields are required' }, 400);
     }
     next();
 }
